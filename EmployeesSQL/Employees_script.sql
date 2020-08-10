1407bdc400e962271d101f246353eb20c73aeeb8
James Cadigan <64744915+jamescadigan97@users.noreply.github.com>
2020-08-10 02:43:12 -0400

    Add files via upload

---
+++ EmployeesSQL/Employees_script.sql
@@ -0,0 +1,118 @@
+--Create tables
+create table employees (
+emp_no integer,
+emp_title_id varchar,
+birth_date date,
+first_name varchar,
+last_name varchar,
+sex varchar,
+hire_date date
+);
+
+CREATE TABLE "departments" (
+dept_no varchar,
+dept_name varchar
+);
+
+CREATE TABLE "salaries" (
+emp_no integer,   
+salary integer
+);
+
+CREATE TABLE "dept_emp" (
+emp_no integer,
+dept_no varchar
+);
+
+CREATE TABLE "department_manager" (
+dept_no varchar,
+emp_no integer
+);
+
+CREATE TABLE "titles" (
+title_id varchar,
+title varchar
+);
+
+-- Create primary and foreign keys
+Alter table employees
+add Primary Key (emp_no);
+
+alter table departments
+add primary key (dept_no)
+
+ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "department_manager" ADD CONSTRAINT "fk_department_manager_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "department_manager" ADD CONSTRAINT "fk_department_manager_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "departments" ("dept_no");
+
+ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "departments" ("dept_no");
+
+--1. List the following details of each employee: employee number, last name, first name, sex, and salary.
+CREATE VIEW employee_salaries AS
+select employees.emp_no, employees.last_name, employees.first_name, employees.sex, salaries.salary
+from employees
+left outer join salaries
+on employees.emp_no = salaries.emp_no;
+
+--2. List first name, last name, and hire date for employees who were hired in 1986.
+Create view employee_1986 as
+select employees.first_name, employees.last_name, employees.hire_date
+from employees
+where hire_date between '1985-12-31' and '1987-01-01'
+
+--3. List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
+create view department_managers_info as
+select departments.dept_no, departments.dept_name, department_manager.emp_no, employees.last_name, employees.first_name
+from department_manager
+left outer join departments
+on departments.dept_no = department_manager.dept_no
+inner join employees
+on employees.emp_no = department_manager.emp_no
+
+--4. List the department of each employee with the following information: employee number, last name, first name, and department name.
+create view employee_department_info as
+select dept_emp.emp_no, employees.last_name, employees.first_name, departments.dept_name
+from dept_emp
+left outer join departments
+on departments.dept_no = dept_emp.dept_no
+right outer join employees
+on employees.emp_no = dept_emp.emp_no
+
+--5. List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
+create view Hercules_B_info as
+select first_name, last_name, sex
+from employees
+where first_name = 'Hercules'
+and last_name LIKE 'B%'
+
+--6. List all employees in the Sales department, including their employee number, last name, first name, and department name.
+create view Sales_development_info as
+select dept_emp.emp_no, employees.last_name, employees.first_name, departments.dept_name
+from dept_emp
+left outer join departments
+on departments.dept_no = dept_emp.dept_no
+right outer join employees
+on employees.emp_no = dept_emp.emp_no
+where departments.dept_name = 'Sales' or
+departments.dept_name = 'Development'
+
+--7. List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+create view last_name_count as
+select last_name, count(last_name) as lastname_cnt from employees
+group by last_name
+order by lastname_cnt desc
+
+--8. In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
+create view last_name_count as
+select last_name, count(last_name) as lastname_cnt from employees
+group by last_name
+order by lastname_cnt desc
